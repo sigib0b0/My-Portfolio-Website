3f523bc04b4fb48cac605811f30f8ed7cab14ea6
Christian Onishile <chrisonish@gmail.com>
2023-07-22 16:26:16 -0400

    Start the about me page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,24 @@ import './about.css'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className='container about__container'>
+        <div className="about__me">
+          <div className="about__me-image">
+            
+          </div>
+
+        </div>
+
+        <div className="about__content">
+
+        </div>
+    
+      </div>
+    </section>
   )
 }
 
